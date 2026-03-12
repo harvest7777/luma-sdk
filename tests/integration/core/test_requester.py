c6f0f7dd4e83395cbffeabbe32098928aa9adebd
harvest7777 <333harvest.moon333@gmail.com>
2026-03-12 13:35:22 -0700

    test: add integration tests for Requester with VCR cassettes

---
+++ tests/integration/core/test_requester.py
@@ -0,0 +1,18 @@
+import pytest
+from core.requester import Requester
+
+
+@pytest.mark.vcr
+def test_successful_request_returns_data():
+    req = Requester(base_url="https://jsonplaceholder.typicode.com")
+    data = req.request_json_and_check("GET", "/todos/1")
+    assert data["id"] == 1
+
+@pytest.mark.vcr
+def test_request_with_x_api_key_header_returns_success():
+    req = Requester(
+        base_url="https://jsonplaceholder.typicode.com",
+        headers={"x-luma-api-key": "test-key"},
+    )
+    data = req.request_json_and_check("GET", "/posts/10")
+    assert data["id"] == 10
