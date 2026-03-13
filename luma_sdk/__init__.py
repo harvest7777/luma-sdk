dd20df1868bdde2ef3484652028c8af94205017a
harvest7777 <333harvest.moon333@gmail.com>
2026-03-12 21:10:15 -0700

    feat: export all exception classes from package __init__

--- luma_sdk/__init__.py
+++ luma_sdk/__init__.py
@@ -1,5 +1,25 @@
 from luma_sdk.luma_client import LumaClient
 from luma_sdk.models.event import Event
+from luma_sdk.models.guest import Guest
 from luma_sdk.paginated_list import PaginatedList
+from luma_sdk.exceptions import (
+    ApiError,
+    NotFoundError,
+    ForbiddenError,
+    ClientError,
+    ServerError,
+    RequestTimeoutError,
+)
 
-__all__ = ["LumaClient", "Event", "PaginatedList"]
+__all__ = [
+    "LumaClient",
+    "Event",
+    "PaginatedList",
+    "Guest",
+    "ApiError",
+    "NotFoundError",
+    "ForbiddenError",
+    "ClientError",
+    "ServerError",
+    "RequestTimeoutError",
+]
