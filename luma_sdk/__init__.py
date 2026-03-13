1c712c7cd913d0f0a90482fda98d1fa59c0dd519
harvest7777 <333harvest.moon333@gmail.com>
2026-03-12 20:27:57 -0700

    feat: export Event from top-level package

--- luma_sdk/__init__.py
+++ luma_sdk/__init__.py
@@ -1,4 +1,5 @@
 from luma_sdk.luma_client import LumaClient
+from luma_sdk.models.event import Event
 from luma_sdk.paginated_list import PaginatedList
 
-__all__ = ["LumaClient", "PaginatedList"]
+__all__ = ["LumaClient", "Event", "PaginatedList"]
