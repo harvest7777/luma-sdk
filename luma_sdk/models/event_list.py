ce98308453a9d0d8f2b78fbc3a2db73456e49a02
harvest7777 <333harvest.moon333@gmail.com>
2026-03-12 20:09:14 -0700

    fix: dead code

--- luma_sdk/models/event_list.py
+++ luma_sdk/models/event_list.py
@@ -10,26 +10,5 @@ class EventList:
             model_cls=Event
         )
     """
-    def __init__(self, requester, params=None):
-        self._requester = requester
-        self._params = params or {}
-
-        self._elements = []
-        self._next_cursor = None
-        self._has_more = True
-
-    def _fetch_next_page_and_update_states(self):
-        pass
-
-    def __iter__(self):
-        index = 0
-
-        while True:
-            if index < len(self._elements):
-                yield self._elements[index]
-                index += 1
-
-            elif self._has_more:
-                self._fetch_next_page_and_update_states()
-            else:
-                break
+    def __init__(self, requester, path, params):
+        raise NotImplementedError
